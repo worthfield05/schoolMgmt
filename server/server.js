a0592c0dd774c423ddccfff1db30dfebc2df0de5
worthfield <karkianish05@gmail.com>
2026-02-02 21:54:22 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+import app from './app.js'
+import  { dbConnect } from './config/dbConnection.js'
+dotenv.config({path:"server/config/config.env"})
+const PORT = 5000
+app.listen(PORT, () => {
+  console.log("Server is running on port ", PORT);
+  dbConnect();
+});
